9cc1e0667bd4c8efedaf36d16d45af9333c9a33f
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2026-01-04 18:23:06 +0200

    remove int total item in order

--- src/main/java/com/lilyhien/model/Order.java
+++ src/main/java/com/lilyhien/model/Order.java
@@ -48,7 +48,6 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> items = new ArrayList<>();;
 
-    private int totalItem;
     private Long totalPrice;
 
 }
