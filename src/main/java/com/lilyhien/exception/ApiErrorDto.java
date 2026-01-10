0c684769df76005f798b4443119286eda03471e6
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2026-01-10 16:56:15 +0200

    add     @JsonFormat to make the format of timestamp less messy

--- src/main/java/com/lilyhien/exception/ApiErrorDto.java
+++ src/main/java/com/lilyhien/exception/ApiErrorDto.java
@@ -1,5 +1,6 @@
 package com.lilyhien.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
@@ -8,7 +9,10 @@ import java.time.LocalDateTime;
 //Spring docs: https://docs.spring.io/spring-boot/3.4/api/java/org/springframework/boot/web/servlet/error/DefaultErrorAttributes.html
 @Data
 public class ApiErrorDto {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
+
     private int statusCode;
     private String error; //The error reason
     private String message; //The exception message
